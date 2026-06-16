2d7880cb47006af2db7ab617a2c574f42c241ca7
Gabriel Ribeiro de Menezes <gabriel.r.menezes@aluno.senai.br>
2026-06-16 10:55:28 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const express = require('express')
-
 const app = express()
 
-app.get('/', (req, res) => {
-  res.json({
-    ok: true
-  })
+const logger = require('./middleware/logger')
+
+app.use(logger)
+
+app.get('/', (req,res)=>{
+  res.json({ ok:true })
 })
 
-const logger = require('./middleware/logger')
-app.use(logger)
 module.exports = app

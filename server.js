3deb7c6d8f98657c668890dfa6b920f5fea88f61
Gabriel Ribeiro de Menezes <gabriel.r.menezes@aluno.senai.br>
2026-06-16 10:48:04 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,33 +3,51 @@ require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 
-const logger =
-    require('./middleware/logger')
+const logger = require('./middleware/logger')
+const errorHandler = require('./middleware/errorHandler')
 
-const errorHandler =
-    require('./middleware/errorHandler')
+const selecoesRouter = require('./routers/selecoes')
+const arbitrosRouter = require('./routers/arbitros')
+const estadiosRouter = require('./routers/estadios')
+const jogosRouter = require('./routers/jogos')
+const avaliacoesRouter = require('./routers/avaliacoes')
 
 const app = express()
 
 app.use(cors())
+
 app.use(express.json())
+
 app.use(logger)
 
-app.use('/selecoes',
-    require('./routers/selecoes'))
+app.get('/', (req, res) => {
 
-app.use('/arbitros',
-    require('./routers/arbitros'))
+    res.status(200).json({
+        projeto: 'Central do Apito',
+        status: 'online',
+        versao: '1.0.0'
+    })
 
-app.use('/estadios',
-    require('./routers/estadios'))
+})
 
-app.use('/jogos',
-    require('./routers/jogos'))
+app.use('/selecoes', selecoesRouter)
 
-app.use('/avaliacoes',
-    require('./routers/avaliacoes'))
+app.use('/arbitros', arbitrosRouter)
+
+app.use('/estadios', estadiosRouter)
+
+app.use('/jogos', jogosRouter)
+
+app.use('/avaliacoes', avaliacoesRouter)
 
 app.use(errorHandler)
 
-module.exports = app
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+
+    console.log(
+        `Servidor rodando na porta ${PORT}`
+    )
+
+})
